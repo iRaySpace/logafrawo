f252b9bc53188ab7d1850e5e027e2f43e9fe862e
Ivan Ray Altomera <iRaySpacii@gmail.com>
2014-05-31 12:01:01 +0800

    added fps and objects counter

--- core/Debug.lua
+++ core/Debug.lua
@@ -2,10 +2,16 @@ local class = require 'libs.middleclass'
 
 Debug = class('Debug')
 
-function Debug:initialize()
+function Debug:initialize(game)
 
 	-- Debug mode defaults to false
 	self.enabled = false
+
+	-- Making a reference to the game
+	self.game = game
+
+	-- Counters
+	self.objectsCounter = self.game:numOfWorldObjects()
 
 end
 
@@ -16,6 +22,11 @@ end
 
 function Debug:draw()
 
+	love.graphics.print('Objects: ' .. self.objectsCounter, 0, 0)
+	love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 0, 16)
 
+end
 
+function Debug:updateC()
+	self.objectsCounter = self.game:numOfWorldObjects()
 end
